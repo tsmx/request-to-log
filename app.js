5dbf3d3734318504205ef0cbdebfab2c229e5bf5
tsmx <dev@tsmx.net>
2025-01-28 20:20:23 +0100

    Introduced body-parser for handling JSON etc

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const logger = require('./logging');
 
 function requestInfoString(req) {
@@ -7,9 +9,22 @@ function requestInfoString(req) {
 };
 
 var app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     logger.info('GET 200 ' + requestInfoString(req));
+    res.sendStatus(200);
+});
+
+app.put('/', (req, res) => {
+    logger.info('PUT 200 ' + requestInfoString(req));
+    logger.info(JSON.stringify(req.body));
+    res.sendStatus(200);
+});
+
+app.post('/', (req, res) => {
+    logger.info('POST 200 ' + requestInfoString(req));
+    logger.info(JSON.stringify(req.body));
     res.sendStatus(200);
 });
 
